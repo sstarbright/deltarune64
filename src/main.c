1150a7a58a7807f323e91d75ffa56da885386921
niaarchangel <explodingspears@gmail.com>
2025-03-07 17:17:47 -0330

    fixed debug issue

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@ float get_time_s()  { return (float)((double)get_ticks_ms() / 1000.0); }
 
 int main(void)
 {
+    debug_init_isviewer();
+    debug_init_usblog();
     dfs_init(DFS_DEFAULT_LOCATION);
     asset_init_compression(2);
     display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS);
@@ -14,7 +16,7 @@ int main(void)
     t3d_init((T3DInitParams){});
     cosmesh_init();
     model_cache_create(1);
-    load_model_into_cache("rom:/mdl_test.t3dm", "Jevil");
+    load_model_into_cache("rom:/mdl_jevil.t3dm", "Jevil");
     
     T3DViewport viewport = t3d_viewport_create();
     
@@ -33,15 +35,20 @@ int main(void)
     module_init(jevil_mesh);
     RenderableModule* jevil_render = (RenderableModule*)jevil_mesh->data;
     Trans3DModule* jevil_trans = (Trans3DModule*)jevil_mesh->next->data;
-    jevil_trans->scale.x = 0.8f;
+    /*jevil_trans->scale.x = 0.8f;
     jevil_trans->scale.y = 0.8f;
-    jevil_trans->scale.z = 0.8f;
+    jevil_trans->scale.z = 0.8f;*/
 
     rdpq_font_t* fnt = rdpq_font_load_builtin(FONT_BUILTIN_DEBUG_MONO);
     rdpq_font_style(fnt, 1, &(rdpq_fontstyle_t){RGBA32(0xAA, 0xAA, 0xFF, 0xFF)});
     rdpq_text_register_font(FONT_BUILTIN_DEBUG_MONO, fnt);
 
     debug_log_init();
+
+    char full_buf[30];
+    snprintf(full_buf, 20, "%f", jevil_trans->scale.x);
+    strcat(full_buf, "\n");
+    debugf(full_buf);
 
     color_t test_light = (color_t){0xFF, 0x00, 0x00, 0xFF};
 
@@ -57,7 +64,7 @@ int main(void)
         t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(85.0f), 10.0f, 150.0f);
         t3d_viewport_look_at(&viewport, &camPos, &camTarget, &(T3DVec3){{0,1,0}});
 
-        actor_life(actor, deltaTime, matrixIdx);
+        actor_life(jevil, deltaTime, matrixIdx);
 
         rdpq_attach(display_get(), display_get_zbuf());
         t3d_frame_start();
@@ -71,7 +78,10 @@ int main(void)
         rdpq_set_prim_color((color_t){0x00, 0x00, 0x00, 0xFF});
         t3d_light_set_ambient(colorAmbient);
 
-        actor_render->draw(actor_render, deltaTime, matrixIdx);
+        jevil_render->draw(jevil_render, deltaTime, matrixIdx);
+
+        rdpq_sync_pipe();
+        debug_log_draw();
 
         rdpq_detach_show();
     }
